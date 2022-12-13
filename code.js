f84df2ff0b1b77d290a0e7974ac131b089502853
Joe Delfino <jdelfino@gmail.com>
2022-12-13 08:48:19 -0500

    Day 0: Drawing circles

--- code.js
+++ code.js
@@ -28,15 +28,28 @@ function draw() {
   c = 0.004;
   d = 1;
   e = 0;
-
-  // This line creates a "loop"
-  // The code inside the { } brackets will run repeatedly, with a
-  // different value of x each time.
-  // The loop starts at x = -250. Each time the loop runs, x increases
-  // by 10. The loop stops when x gets to 250.
-  for (var x = -250; x < 250; x += 10) {
-
-    // This line draws a circle at point (x, f(x)), with diameter 5.
-    circle(x, f(x), 5);
-  }
+  
+  // Each of these lines draws a circle at a point, with a radius.
+  // circle(10, 20, 5) draws a circle at point (10, 20) with radius 5.
+  circle(-200, f(-200), 5);
+  circle(-180, f(-180), 5);
+  circle(-160, f(-160), 5);
+  circle(-140, f(-140), 5);
+  circle(-120, f(-120), 5);
+  circle(-100, f(-100), 5);
+  circle(-80, f(-80), 5);
+  circle(-60, f(-60), 5);
+  circle(-40, f(-40), 5);
+  circle(-20, f(-20), 5);
+  circle(0, f(0), 5);
+  circle(20, f(20), 5);
+  circle(40, f(40), 5);
+  circle(60, f(60), 5);
+  circle(80, f(80), 5);
+  circle(100, f(100), 5);
+  circle(120, f(120), 5);
+  circle(140, f(140), 5);
+  circle(160, f(160), 5);
+  circle(180, f(180), 5);
+  circle(200, f(200), 5);  
 }
